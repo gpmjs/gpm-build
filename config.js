aeb1e4a5ee76c5ec81fabb325b2a8f49fdee64dd
chenzhuxing <Zhuxing.chen@qicai360.cn>
2016-08-22 17:18:05 +0800

    gpm-bulid 项目

---
+++ config.js
@@ -0,0 +1,16 @@
+/*
+ *  打包配置文件
+ *
+ */
+
+'use strict';
+
+var dist = './test/dist';
+
+var config = {
+	clean: {
+		dist: dist
+	}
+};
+
+module.exports = config;
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/*
+ *  gulp文件
+ *
+ */
+'use strict';
+
+var requireDir = require('require-dir');
+
+requireDir('./lib/tasks', { recurse: true});
