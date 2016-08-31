c993817ffff31c956d2144d86a20f19ad85114cf
chenzhuxing <Zhuxing.chen@qicai360.cn>
2016-08-31 23:26:19 +0800

    更新环境

--- gpmfile.js
+++ gpmfile.js
@@ -6,8 +6,8 @@
 module.exports = function(gpm) {
 	'use strict';
 
-	var src = './test/src';
-	var dest = './test/dist';
+	var src = '/test/src';
+	var dest = '/test/dist';
 
 	// 定义各种tasks 参数
 	gpm.initConfig({
@@ -16,35 +16,50 @@ module.exports = function(gpm) {
 		},
 		concat: {
 			cssConfig: {
-				src: src + '/css/*.css',
+				src: src + '/css/**/*.css',
 				dest: dest + '/css',
 				fileName: 'normal.css'
 			},
 			jsConfig: {
-				src: src + '/js/*.js',
+				src: src + '/js/**/*.js',
 				dest: dest + '/js',
 				fileName: 'turtle.js'
 			}
 		},
 		minify: {
 			cssConfig: {
-				src: src + '/css/*.css',
+				src: src + '/css//**/*.css',
 				dest: dest + '/css',
 				fileName: 'normal.min.css'
 			},
 			jsConfig: {
-				src: src + '/js/*.js',
+				src: src + '/js//**/*.js',
 				dest: dest + '/js',
 				fileName: 'turtle.min.js'
 			}
 		},
 		images: {
-			src: src + '/images/*',
+			src: src + '/images/**/*',
 			dest: dest + '/images'
+		},
+		rev: {
+			revJson: dest + '/rev/**/*.json',
+			dest: dest + '/rev',
+			htmlSrc: './test/*.html',
+			htmlDest: './test'
+		},
+		ts: {
+			src: ['/test/**/*.html'],
+			dirpath: '/test',
+			fiter: ['**/*.html']
+		},
+		release: {
+			path: '/package.json'
 		}
 	});
 
 	// 定义具体的任务
-	gpm.registerTask('dev', ['less']);
-	gpm.registerTask('online', ['watch']);
+	gpm.registerTask('dev', ['ts', 'release']);
+	gpm.registerTask('qc', ['ts', 'release']);
+	gpm.registerTask('online', ['ts', 'release']);
 }
